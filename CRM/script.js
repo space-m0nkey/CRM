18fcbf5e449d3ba79bca13686b09871f80cf9019
Gabriel.Almeida-10 <105819653+space-m0nkey@users.noreply.github.com>
2024-09-04 15:10:24 -0300

    Add files via upload

---
+++ CRM/script.js
@@ -0,0 +1,93 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const container = document.getElementById('expandableContainer1');
+    
+    container.addEventListener('click', () => {
+        container.classList.toggle('expanded');
+    });
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+    const container = document.getElementById('expandableContainer2');
+    
+    container.addEventListener('click', () => {
+        container.classList.toggle('expanded');
+    });
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+    const container = document.getElementById('expandableContainer3');
+    
+    container.addEventListener('click', () => {
+        container.classList.toggle('expanded');
+    });
+});
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    const container = document.getElementById('expandableContainer4');
+    
+    container.addEventListener('click', () => {
+        container.classList.toggle('expanded');
+    });
+});
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    const container = document.getElementById('expandableContainer5');
+    
+    container.addEventListener('click', () => {
+        container.classList.toggle('expanded');
+    });
+});
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    const container = document.getElementById('expandableContainer6');
+    
+    container.addEventListener('click', () => {
+        container.classList.toggle('expanded');
+    });
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+    const container = document.getElementById('expandableContainer7');
+    
+    container.addEventListener('click', () => {
+        container.classList.toggle('expanded');
+    });
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+    const container = document.getElementById('expandableContainer8');
+    
+    container.addEventListener('click', () => {
+        container.classList.toggle('expanded');
+    });
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+    const container = document.getElementById('expandableContainer9');
+    
+    container.addEventListener('click', () => {
+        container.classList.toggle('expanded');
+    });
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+    const container = document.getElementById('expandableContainer10');
+    
+    container.addEventListener('click', () => {
+        container.classList.toggle('expanded');
+    });
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+    const container = document.getElementById('expandableContainer11');
+    
+    container.addEventListener('click', () => {
+        container.classList.toggle('expanded');
+    });
+});
+
+
+
